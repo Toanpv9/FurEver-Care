6e0c267c7fa7c94c51ab7cbf33e668a4088f08bd
Toanpv9 <phamviettoan996@gmail.com>
2025-09-11 14:32:03 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";    
 import Header from "./components/Header"; 
-import "./styles/tiny-slider.css";
+import Footer from "./components/Footer";
+import AboutUs from "./pages/AboutUs";
+import CatFood from "./pages/Catfood";
+import DogFood from "./pages/Dogfood";
+
 
 
 function App() {
@@ -11,12 +15,49 @@ function App() {
       <NavBar />
 
       <Routes>
+        {/* Home chỉ load Hero */}
         <Route path="/" element={<Hero />} />
-        <Route path="/properties" element={<Header title="Properties" background="images/hero_bg_1.jpg" />} />
-        <Route path="/services" element={<Header title="Services" background="images/hero_bg_1.jpg" />} />
-        <Route path="/about" element={<Header title="About Us" background="images/hero_bg_2.jpg" />} />
-        <Route path="/contact" element={<Header title="Contact Us" background="images/hero_bg_1.jpg" />} />
+        <Route path="/aboutus" element={<AboutUs />} />
+
+        {/* CatFood và DogFood */}
+        <Route path="/catfood" element={<CatFood />} />
+        <Route path="/dogfood" element={<DogFood />} />
+
+        
+
+
+        {/* Các trang khác */}
+        <Route 
+          path="/petcare" 
+          element={<Header title="Pet Care" background="images/hero_bg_1.jpg" />} 
+        />
+        <Route 
+          path="/products" 
+          element={<Header title="Products" background="images/hero_bg_2.jpg" />} 
+        />
+        <Route 
+          path="/emergency" 
+          element={<Header title="Emergency Help" background="images/hero_bg_1.jpg" />} 
+        />
+        <Route 
+          path="/veterinarians" 
+          element={<Header title="Veterinarians" background="images/hero_bg_2.jpg" />} 
+        />
+        <Route 
+          path="/petshelter" 
+          element={<Header title="Pet Shelter" background="images/hero_bg_1.jpg" />} 
+        />
+        <Route 
+          path="/feedback" 
+          element={<Header title="Feedback" background="images/hero_bg_2.jpg" />} 
+        />
+        <Route 
+          path="/contact" 
+          element={<Header title="Contact Us" background="images/hero_bg_1.jpg" />} 
+        />
       </Routes>
+
+      <Footer />
     </Router>
   );
 }
